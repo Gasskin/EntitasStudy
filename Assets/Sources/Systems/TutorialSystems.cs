13a60399d83fee174b62c37871d8aac9ffd0e73f
BANGING\LJW <liujiawei@banging.com.cn>
2021-01-25 10:37:37 +0800

    HelloWorld

---
+++ Assets/Sources/Systems/TutorialSystems.cs
@@ -0,0 +1,12 @@
+﻿// TutorialSystems.cs
+public class TutorialSystems : Feature
+{
+    // 添加所有要用到的System，base里面是调试节点的名字
+    public TutorialSystems(Contexts contexts) : base("Tutorial Systems")
+    {
+        Add (new HelloWorldSystem(contexts));
+        Add (new LogMouseClickSystem (contexts));
+        Add (new DebugMessageSystem(contexts));
+        Add (new CleanupDebugMessageSystem (contexts));
+    }
+}
